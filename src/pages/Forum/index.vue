0b729e7654ac91e0cf374c314cf92a4b2b7c4933
“xlsssss” <1970029934@qq.com>
2022-08-20 14:41:17 +0800

    new

---
+++ src/pages/Forum/index.vue
@@ -0,0 +1,23 @@
+<template>
+<view>
+	<Search></Search>
+</view>
+</template>
+
+<script>
+ import {Search} from '../../components/Search.vue'
+	export default{
+		data(){
+			return{
+
+		}
+	},
+	components:{
+		Search
+	}
+}
+<script>
+
+<style>
+
+</style>
